8b95a5ecff68f68d63914aeae1a82610827f25f3
AL - ARAFAT (  YEASH  ) <149018537+researcher-program@users.noreply.github.com>
2025-12-09 17:49:47 +0600

    Add files via upload
    This repo as a formal or basic on TS and showing similarities on JS and TS.

---
+++ basic-1.ts
@@ -0,0 +1,21 @@
+function greet(name: string):string {
+    return `hello ${name}`
+}
+    console.log(greet("Aafat"));
+    console.log(greet(true));
+    console.log(greet(42));   
+
+
+/*
+1. When to learn TS ? =>When Over in JS,that means you well-know about JS.
+2. What is TS ?=> TS is giving superPower in JS,That means,JS Types,means DataTypes... 
+
+Some Issues :
+i.Freedom 
+ii.Loose-->JSDocs
+iii.Developer Tooling
+iv. AI
+
+TS Pros:i.Addon,ii.No running tool,or not run it,it can need process to give output :JS,like: TS --> Process --> JS
+iii.features:type Checker {Consistency}
+*/
